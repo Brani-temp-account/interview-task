4479a84d25e5f8e1fa9423d28423d896fbfa29ea
Brani-temp-account <cr4b007@gmail.com>
2025-05-30 07:20:55 +0200

    setup changes: -BE handlers adjusted for future date -material icon fonts added -logo added -locale registered -angular material added -global styles setup -variables.scss added for future use

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
-const start = () =>
-  bootstrapApplication(AppComponent, appConfig);
+import { registerLocaleData } from '@angular/common';
+import localeSk from '@angular/common/locales/sk';
 
+registerLocaleData(localeSk);
+
+const start = () => bootstrapApplication(AppComponent, appConfig);
 
 import('./mocks/browser').then(({ worker }) => {
   worker.start({ onUnhandledRequest: 'bypass' }).then(() => {
