69d04618ee9a505b08e31670308a001470b46dd7
Brani-temp-account <cr4b007@gmail.com>
2025-05-30 07:47:07 +0200

    Usage of all the step components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,30 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HeaderComponent } from './components/parts/header/header.component';
+import { NavigationService, Steps } from './services/navigation.service';
+import { TimeslotComponent } from './components/steps/timeslot/timeslot.component';
+import { PersonalDataComponent } from './components/steps/personal-data/personal-data.component';
+import { SummaryComponent } from './components/steps/summary/summary.component';
+import { FinalizationComponent } from './components/steps/finalization/finalization.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    TimeslotComponent,
+    PersonalDataComponent,
+    SummaryComponent,
+    FinalizationComponent,
+  ],
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  protected readonly Steps = Steps;
+
+  private navigationService = inject(NavigationService);
+
+  currentStep = this.navigationService.currentStep;
 }
