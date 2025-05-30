76567062cc166442e010eb90768f90be20475dba
Brani-temp-account <cr4b007@gmail.com>
2025-05-30 07:58:01 +0200

    Resetting the app when clicking browser back button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { HeaderComponent } from './components/parts/header/header.component';
 import { NavigationService, Steps } from './services/navigation.service';
 import { TimeslotComponent } from './components/steps/timeslot/timeslot.component';
@@ -21,10 +21,28 @@ import { FinalizationComponent } from './components/steps/finalization/finalizat
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   protected readonly Steps = Steps;
 
   private navigationService = inject(NavigationService);
 
   currentStep = this.navigationService.currentStep;
+
+  ngOnInit() {
+    // Necessary to have a state to check when popstate fires
+    history.pushState(null, '', location.href);
+
+    window.addEventListener('popstate', (event) => {
+      if (
+        this.navigationService.currentStep() !==
+        this.navigationService.startingStep
+      ) {
+        // Used for simple reset of the page
+        location.reload();
+        console.log('resetting');
+      } else {
+        console.log('going back');
+      }
+    });
+  }
 }
